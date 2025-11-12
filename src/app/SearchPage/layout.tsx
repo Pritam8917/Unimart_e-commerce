c30be9402f5a8fa07968d1c311832cc029764465
Pritam Das Adhikari <pritamdasadhikari8917@gmail.com>
2025-11-12 21:59:44 +0530

    update searchlayout

---
+++ src/app/SearchPage/layout.tsx
@@ -0,0 +1,14 @@
+
+import { Suspense } from "react";
+
+export default function SearchLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Suspense fallback={<p>Loading search results...</p>}>
+      {children}
+    </Suspense>
+  );
+}
